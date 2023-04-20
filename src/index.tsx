68c33f413ad0b293c8dd6ee1b334f24b6bf5a12e
rwormsbecher <rodney22@live.nl>
2023-04-20 18:43:11 +0200

    update to export types for leasifybutton

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,13 @@
-export { Button } from './Button';
+
+// Leasify Button
 export { Button as LeasifyButton} from "./components/Button/Button"
+export { 
+    ButtonTypesEnum as LeasifyButtonTypesEnum, 
+    ButtonSizesEnum as LeasifyButtonSizesEnum, 
+    ButtonHorizontalPositionEnum as LeasifyButtonHorizontalPositionEnum 
+} from "./components/Button/Button"
+
+
+// Themes
+export { generalTheme} from "./themes/generalTheme"
+export { IThemeModel } from "./models/IThemeModel"
